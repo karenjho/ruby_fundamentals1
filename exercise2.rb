587544799110418e7b7cf0466ebc762792404522
karenjho <hokarenj@gmail.com>
2016-01-12 14:30:46 -0500

    Create Exercise2 with good tip calculator

---
+++ exercise2.rb
@@ -0,0 +1,12 @@
+#How would you calculate a good tip for a 55 dollar meal?
+
+#First, define a variable for the total meal price.
+#Set the total meal price at 55
+meal_price = 55
+
+#Next, calculate the percentage tip for the meal.
+#Set the tip percentage at 20%
+good_tip = 0.2
+
+#Finally, use Ruby math to calculate the tip for the meal.
+puts "A good tip for a $#{meal_price} meal is $#{meal_price * good_tip}."
