e24eb13151be1c64c40be2d0287e8afd89444734
karenjho <hokarenj@gmail.com>
2016-01-12 14:53:26 -0500

    Add age calculator to exercise3

--- exercise3.rb
+++ exercise3.rb
@@ -1,4 +1,10 @@
 puts "What is your name?"
-name = gets.chomp
+name = gets.chomp.capitalize
 
 puts "Hello, #{name}. Nice to meet you!"
+
+puts "What year were you born, #{name}?"
+year = gets.chomp.to_i
+age = 2016 - year
+
+puts "#{name}, you are #{age - 1} or #{age} years old."
